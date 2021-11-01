c6658f14ea6994395e856e172b953c20dad15cf2
Restu  Fajar Sidhiq <64077625+refs05@users.noreply.github.com>
2021-11-02 06:47:20 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactUs from './components/contactUs';
+import NavbarHome from './components/navbarHome';
+import SectionHome from './components/sectionHome';
+
+const App = () => (
+    <React.Fragment>
+      {/* <NavbarHome/>
+      <SectionHome/> */}
+      <ContactUs/>
+    </React.Fragment>
+);
+
+export default App;
